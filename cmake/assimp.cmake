e5b2298b8602c350b3f78ae40d5006af4d13a16a
myhql <1051576073@qq.com>
2022-01-13 22:42:22 +0800

    [edit]修正三方库导入

--- cmake/assimp.cmake
+++ cmake/assimp.cmake
@@ -5,14 +5,15 @@ if(${CMAKE_VERSION} VERSION_LESS 3.14)
     include(add_FetchContent_MakeAvailable.cmake)
 endif()
 
-set(LIB_NAME  glfw)  # 指定版本
-set(LIB_GIT_TAG  3.3.6)  # 指定版本
-set(LIB_GIT_URL  https://github.com/glfw/glfw.git)  # 指定git仓库地址
+set(LIB_NAME  assimp)  # 指定版本
+set(LIB_GIT_TAG  v5.1.6)  # 指定版本
+set(LIB_GIT_URL  https://github.com/assimp/assimp.git)  # 指定git仓库地址
 
-set(GLFW_BUILD_EXAMPLES OFF)
-set(GLFW_BUILD_TESTS OFF)
-set(GLFW_BUILD_DOCS OFF)
-set(GLFW_INSTALL OFF)
+set(ASSIMP_BUILD_ASSIMP_TOOLS OFF)
+set(ASSIMP_BUILD_SAMPLES OFF)
+set(ASSIMP_BUILD_TESTS OFF)
+
+set(CMAKE_POLICY_DEFAULT_CMP0077 NEW)
 
 FetchContent_Declare(
   ${LIB_NAME}
@@ -20,5 +21,7 @@ FetchContent_Declare(
   GIT_TAG           ${LIB_GIT_TAG}
   SOURCE_DIR        ${PROJECT_SOURCE_DIR}/thirdparty/${LIB_NAME}
 )
+
+
 
 FetchContent_MakeAvailable(${LIB_NAME})
