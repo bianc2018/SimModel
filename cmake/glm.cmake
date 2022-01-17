eefd72ebcd427ead2b21266f3787f17d0c38d8f3
myhql <1051576073@qq.com>
2022-01-17 22:29:15 +0800

    [add]添加glm模块

---
+++ cmake/glm.cmake
@@ -0,0 +1,2 @@
+set(LIB_NAME  glm)  
+add_subdirectory(${PROJECT_SOURCE_DIR}/thirdparty/${LIB_NAME})
--- src/SimGL.cpp
+++ src/SimGL.cpp
@@ -1,6 +1,23 @@
 #include "SimGL.hpp"
 #include "assimp/scene.h"
 #include "GLFW/glfw3.h"
+#include "glm/glm.hpp"
+#include <glm/vec3.hpp> // glm::vec3
+#include <glm/vec4.hpp> // glm::vec4
+#include <glm/mat4x4.hpp> // glm::mat4
+#include <glm/ext/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale
+#include <glm/ext/matrix_clip_space.hpp> // glm::perspective
+#include <glm/ext/scalar_constants.hpp> // glm::pi
+
+glm::mat4 camera(float Translate, glm::vec2 const& Rotate)
+{
+    glm::mat4 Projection = glm::perspective(glm::pi<float>() * 0.25f, 4.0f / 3.0f, 0.1f, 100.f);
+    glm::mat4 View = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -Translate));
+    View = glm::rotate(View, Rotate.y, glm::vec3(-1.0f, 0.0f, 0.0f));
+    View = glm::rotate(View, Rotate.x, glm::vec3(0.0f, 1.0f, 0.0f));
+    glm::mat4 Model = glm::scale(glm::mat4(1.0f), glm::vec3(0.5f));
+    return Projection * View * Model;
+}
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
@@ -25,5 +42,6 @@ int run_main(int argc, char* argv[])
         glfwTerminate();
         return -1;
     }
+    camera(1.0f, glm::vec2(1.0f, 1.0f));
     return 0;
 }
