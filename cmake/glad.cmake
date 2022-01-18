5ef29a7819682954a273911e43e08212b21e145f
myhql <1051576073@qq.com>
2022-01-18 21:59:10 +0800

    [add]添加glad和stb_image依赖库

---
+++ cmake/glad.cmake
@@ -0,0 +1,2 @@
+set(LIB_NAME  glad)  
+add_subdirectory(${PROJECT_SOURCE_DIR}/thirdparty/${LIB_NAME})
---
+++ cmake/stb_image.cmake
@@ -0,0 +1,2 @@
+set(LIB_NAME  stb_image)  
+include_directories(${PROJECT_SOURCE_DIR}/thirdparty/${LIB_NAME})
